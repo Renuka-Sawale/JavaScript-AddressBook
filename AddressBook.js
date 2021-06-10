ec9d1c8beab85020753c0d0bf0783615375411da
Renuka-Sawale <rrsuryawanshi11@gmail.com>
2021-06-11 00:48:48 +0530

    Created Address Book Contact

--- AddressBook.js
+++ AddressBook.js
@@ -1,2 +1,38 @@
-// the hello world program
-console.log('Welcome to js address book');
+"use strict";
+const ps = require("prompt-sync")
+const prompt = ps();
+
+class Contact {
+    firstName;
+    lastName;
+    address;
+    city;
+    state;
+    zip;
+    phoneNo;
+    email;
+
+    constructor(firstName, lastName, address, city, state, zip, phoneNo, email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNo = phoneNo;
+        this.email = email;
+    }
+}
+
+let contact = new Contact("Samarh", "Sawale", "Marunji", "Pune", "Maharastra", "5012", "9104223344", "samarth1@gmail.com");
+console.log(contact);
+
+
+
+
+
+
+
+    
+
+
